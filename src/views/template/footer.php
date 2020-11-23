290e521877be0ab8d9943ebae175e618df9a2ee7
Hernande Monteiro <monteiro1998@live.com>
2020-11-23 16:16:54 -0300

    Modify
    Modificando nome de produção

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <span>Desenvolvido com</span>
     <span><i class="icofont-heart text-danger mx-1"></i></span>
-    <span>por COD<span class="text-danger">3</span>R</span>
+    <span>por <a href='http://hernandemonteiro.online'> Hernande Monteiro</a></span>
 </footer>
 <script src="assets/js/app.js"></script>
 </body>
